0a427d3c86f9f5a59657c5d5df5857c0f526665d
leo <lhjang@mybroccoli.co.kr>
2020-06-12 15:15:34 +0900

    cleanup: 불필요코드 삭제

--- gm-core/src/main/java/com/nomadconnection/dapp/core/config/CrownixConfig.java
+++ gm-core/src/main/java/com/nomadconnection/dapp/core/config/CrownixConfig.java
@@ -2,13 +2,11 @@ package com.nomadconnection.dapp.core.config;
 
 import lombok.Getter;
 import lombok.Setter;
-import lombok.experimental.Accessors;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Setter
 @Getter
-@Accessors(fluent = true)
 @Configuration
 @ConfigurationProperties(prefix = "crownix")
 public class CrownixConfig {
