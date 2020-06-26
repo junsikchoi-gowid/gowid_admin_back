eb9ea5a85f76cc8297ecc04c166cefa97982b3b3
JiHeeJeong <wlgml5311@nomadconnection.com>
2020-06-26 14:04:53 +0900

    s3 ec2 버전으로 코드변환

--- gm-api-user/src/main/java/com/nomadconnection/dapp/api/service/AwsS3Service.java
+++ gm-api-user/src/main/java/com/nomadconnection/dapp/api/service/AwsS3Service.java
@@ -1,7 +1,5 @@
 package com.nomadconnection.dapp.api.service;
 
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.auth.InstanceProfileCredentialsProvider;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
@@ -31,8 +29,8 @@ public class AwsS3Service {
     public String s3FileUpload(File file, String key) {
         try {
             AmazonS3 amazonS3 = AmazonS3ClientBuilder.standard()
-                    .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(config.accessKey(), config.secretKey())))
-                    //.withCredentials(new InstanceProfileCredentialsProvider(false))
+                    //.withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(config.accessKey(), config.secretKey())))
+                    .withCredentials(new InstanceProfileCredentialsProvider(false))
                     .withRegion(Regions.AP_NORTHEAST_2)
                     .build();
             amazonS3.putObject(config.bucketName(), key, file);
@@ -67,8 +65,8 @@ public class AwsS3Service {
     public void s3FileDelete(String key) {
         try {
             AmazonS3 amazonS3 = AmazonS3ClientBuilder.standard()
-                    .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(config.accessKey(), config.secretKey())))
-                    //.withCredentials(new InstanceProfileCredentialsProvider(false))
+                    //.withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(config.accessKey(), config.secretKey())))
+                    .withCredentials(new InstanceProfileCredentialsProvider(false))
                     .withRegion(Regions.AP_NORTHEAST_2)
                     .build();
 
