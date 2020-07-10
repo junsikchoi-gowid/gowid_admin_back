4c0297b9ecac2b5e84b62c3879ec9d9f31d96cd1
junsikchoi-nomad <58801430+junsikchoi-nomad@users.noreply.github.com>
2020-07-10 09:22:56 +0900

    Update CardCompany.java

--- gm-core/src/main/java/com/nomadconnection/dapp/core/domain/CardCompany.java
+++ gm-core/src/main/java/com/nomadconnection/dapp/core/domain/CardCompany.java
@@ -6,13 +6,9 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum CardCompany {
-
-	SHINHAN("신한", "1"),
-	LOTTE("롯데", "2"),
-	HYUNDAI("현대", "3"),
-	SAMSUNG("삼성", "4"),
+	SHINHAN("0306"),
+	LOTTE("0311"),
 	;
 
-	private String description;
 	private String code;
 }
