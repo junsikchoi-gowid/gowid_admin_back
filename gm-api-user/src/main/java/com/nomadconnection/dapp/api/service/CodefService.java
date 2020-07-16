101072ae35c6384c988ecd723d487fe7331b4a1c
junsikchoi-nomad <jschoi@nomadconnection.com>
2020-07-16 22:39:57 +0900

    법인등기부등본 - 살아있는 등기 등록 변경

--- gm-api-user/src/main/java/com/nomadconnection/dapp/api/service/CodefService.java
+++ gm-api-user/src/main/java/com/nomadconnection/dapp/api/service/CodefService.java
@@ -936,19 +936,67 @@ public class CodefService {
 					"",
 					"",
 					"",
-					"1",
+					"0",
 					"",
 					"",
 					"",
 					"N"
 			);
-			JSONObject[] jsonObjectCorpRegister = getApiResult(strResult1530);
 
+			JSONObject[] jsonObjectCorpRegister = getApiResult(strResult1530);
 			String jsonObjectCorpRegisterCode = jsonObjectCorpRegister[0].get("code").toString();
+
+			if (jsonObjectCorpRegisterCode.equals("CF-00000")) {
+				JSONObject jsonDataCorpRegister = jsonObjectCorpRegister[1];
+				String resIssueYn = jsonDataCorpRegister.get("resIssueYN").toString();
+				if (resIssueYn.equals("0")) {
+
+					JSONArray jsonDataArraySearchList = (JSONArray) jsonDataCorpRegister.get("resSearchList");
+
+					String registrationNumber = "";
+					for (Object o : jsonDataArraySearchList) {
+						JSONObject obj = (JSONObject) o;
+						if (obj.get("commRegistryStatus").equals("살아있는 등기") && obj.get("commBranchType").equals("본점")) {
+							registrationNumber = obj.get("resRegistrationNumber").toString().trim();
+							break;
+						}
+					}
+
+					strResult1530 = CORP_REGISTER.corp_register(
+							"0002",
+							"0261057000",
+							RSAUtil.encryptRSA("6821", CommonConstant.PUBLIC_KEY),
+							"1", // 검색구분 1 등기번호로 찾기
+							registrationNumber, // 살아있는 등기
+							"1", // 등기사항증명서종류 1 유효한부분
+							"T34029396293",
+							"gowid99!",
+							"",
+							"",
+							"",
+							"",
+							"0",
+							"",
+							"",
+							"",
+							"N"
+					);
+				} else if (!resIssueYn.equals("1")) {
+
+					deleteAccount2(connectedId); // 삭제
+					throw new RuntimeException("발행실패");
+				}
+			}
+
+			jsonObjectCorpRegister = getApiResult(strResult1530);
+			jsonObjectCorpRegisterCode = jsonObjectCorpRegister[0].get("code").toString();
+
 			if (jsonObjectCorpRegisterCode.equals("CF-00000")) {
 				JSONObject jsonDataCorpRegister = jsonObjectCorpRegister[1];
 
 				if (!jsonDataCorpRegister.get("resIssueYN").toString().equals("1")) {
+
+					deleteAccount2(connectedId); // 삭제
 					throw new RuntimeException("발행실패");
 				}
 
