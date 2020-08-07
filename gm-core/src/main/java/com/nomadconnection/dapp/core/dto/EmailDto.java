853250ae9f2322a2e90950d611ce70ebdd899876
레오(장래혁) <58159225+lhjang@users.noreply.github.com>
2020-08-07 12:06:05 +0900

    fix: 이메일 숫자 콤마 추가 (#253)
    Co-authored-by: leo <lhjang@mybroccoli.co.kr>

--- gm-core/src/main/java/com/nomadconnection/dapp/core/dto/EmailDto.java
+++ gm-core/src/main/java/com/nomadconnection/dapp/core/dto/EmailDto.java
@@ -1,15 +1,25 @@
 package com.nomadconnection.dapp.core.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.util.StringUtils;
+
+import java.text.NumberFormat;
 
 @Getter
 @Builder
 @NoArgsConstructor
-@AllArgsConstructor
 public class EmailDto {
+
+	public EmailDto(String licenseNo, String companyName, String hopeLimit, String grantLimit) {
+		long hopeLimitLong = Long.parseLong( StringUtils.isEmpty(hopeLimit) ? "0" : hopeLimit );
+		long grantLimitLong = Long.parseLong( StringUtils.isEmpty(grantLimit) ? "0" : grantLimit );
+		this.licenseNo = licenseNo;
+		this.companyName = companyName;
+		this.hopeLimit =  NumberFormat.getInstance().format(hopeLimitLong);
+		this.grantLimit = NumberFormat.getInstance().format(grantLimitLong);
+	}
 
 	private String licenseNo;
 
