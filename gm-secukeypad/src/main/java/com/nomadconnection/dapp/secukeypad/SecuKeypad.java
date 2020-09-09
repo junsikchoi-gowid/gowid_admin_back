5527f805e53c4a07f7d472a52d69e4993504d6de
다니엘(이창열) <48945426+cylee0227@users.noreply.github.com>
2020-09-09 11:18:39 +0900

    키패드 세션 문제확인을 위한 로깅 추가 (#325)

--- gm-secukeypad/src/main/java/com/nomadconnection/dapp/secukeypad/SecuKeypad.java
+++ gm-secukeypad/src/main/java/com/nomadconnection/dapp/secukeypad/SecuKeypad.java
@@ -2,12 +2,13 @@ package com.nomadconnection.dapp.secukeypad;
 
 import com.nprotect.common.json.JSONParser;
 import com.nprotect.pluginfree.PluginFree;
+import com.nprotect.pluginfree.PluginFreeConst;
 import com.nprotect.pluginfree.modules.PluginFreeRequest;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.HashMap;
-import java.util.Map;
+import javax.servlet.http.HttpSession;
+import java.util.*;
 
 @Slf4j
 public class SecuKeypad {
@@ -19,6 +20,7 @@ public class SecuKeypad {
             Map<String, String> map = (Map<String, String>) new JSONParser().parse(str);
 
             HttpServletRequest decryptRequest = new PluginFreeRequest(request, map);
+            loggingKeyPadSession(request);  // 키패드 세션로깅
             try {
                 PluginFree.verify(decryptRequest, paramNames);
             } catch (Exception e) {
@@ -37,5 +39,31 @@ public class SecuKeypad {
             log.error("[decrypt] $DECRYPT_ERROR({}): {}", e.getClass().getSimpleName(), e.getMessage(), e);
             throw SecuKeypadException.builder().category(SecuKeypadException.Category.DECRYPT_ERROR).data(e.getMessage()).build();
         }
+    }
+
+    public static void loggingKeyPadSession(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        Map<String, String> map3 = (Map<String, String>) session.getAttribute(PluginFreeConst.PLUGINFREE_SESSION_KEY);
+
+        StringBuilder builder = new StringBuilder();
+        builder.append("Print PluginFree Session Start....................................\n");
+
+        if (map3 != null) {
+            Set set = map3.keySet();
+            List<String> list3 = new ArrayList<String>(set);
+            Collections.sort(list3);
+
+            for (String key : list3) {
+                Object value1 = map3.get(key);
+                builder.append("\t" + key + " : " + value1 + "\n");
+            }
+
+        } else {
+            builder.append("Print PluginFree Session is null.\n");
+        }
+
+        builder.append("Print PluginFree Session End...................................\n");
+        log.debug("### PluginFree Session => {} ", builder.toString());
+
     }
 }
