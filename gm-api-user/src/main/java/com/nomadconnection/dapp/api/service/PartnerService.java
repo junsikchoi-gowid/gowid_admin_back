807f15b3d57f7ab77afc0385d097aca12d85a152
팀(현탁) <51900778+ml-hyuntak@users.noreply.github.com>
2020-12-24 17:00:25 +0900

    feat: 외부 연동 방법 변경

--- gm-api-user/src/main/java/com/nomadconnection/dapp/api/service/PartnerService.java
+++ gm-api-user/src/main/java/com/nomadconnection/dapp/api/service/PartnerService.java
@@ -1,30 +1,32 @@
 package com.nomadconnection.dapp.api.service;
 
 import com.nomadconnection.dapp.core.domain.corp.Corp;
-import com.nomadconnection.dapp.core.domain.corp.ExternalCorp;
-import com.nomadconnection.dapp.core.domain.repository.corp.ExternalCorpRepository;
+import com.nomadconnection.dapp.core.domain.user.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Optional;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class PartnerService {
 
-    private final ExternalCorpRepository repoExternalCorp;
+    private final UserService service;
 
     @Transactional
     public Corp getIdxCorp(String externalId) {
-        ExternalCorp externalCorp = repoExternalCorp.findByExternalId(externalId);
+        User user = service.findByExternalId(externalId);
 
-        if(externalCorp == null)
+        if(externalId.equals(user.externalId())) {
             return null;
+        }
 
-        Corp corp = externalCorp.corp();
+        Corp corp = user.corp();
+
+        if(corp == null) {
+            return null;
+        }
 
         log.debug("idxUser {}", corp.user().idx());
 
