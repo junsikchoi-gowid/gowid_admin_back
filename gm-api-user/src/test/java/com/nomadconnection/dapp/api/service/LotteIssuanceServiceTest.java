efd097c1b1e7cf7c5ed191e7888141905a9daa6c
류제용 <69829400+angryong-gowid@users.noreply.github.com>
2021-01-07 19:04:12 +0900

    웰컴메일 템플릿 수정 (#604)
    * 신한카드 발급완료시 웰컴메일 전송
    
    * 메일제목 및 로직 수정
    
    * 메일 템플릿 수정
    
    * 메일 템플릿 수정

--- gm-api-user/src/test/java/com/nomadconnection/dapp/api/service/LotteIssuanceServiceTest.java
+++ gm-api-user/src/test/java/com/nomadconnection/dapp/api/service/LotteIssuanceServiceTest.java
@@ -1,32 +1,39 @@
 package com.nomadconnection.dapp.api.service;
 
-import com.nomadconnection.dapp.api.abstracts.AbstractSpringBootTest;
+import com.nomadconnection.dapp.UserApiApplication;
+import com.nomadconnection.dapp.api.abstracts.AbstractMockitoTest;
 import com.nomadconnection.dapp.api.service.lotte.LotteIssuanceService;
-import com.nomadconnection.dapp.core.domain.corp.Corp;
-import com.nomadconnection.dapp.core.domain.repository.corp.CorpRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.annotation.Order;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @Slf4j
-public class LotteIssuanceServiceTest extends AbstractSpringBootTest {
+@ActiveProfiles("dev")
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = UserApiApplication.class)
+public class LotteIssuanceServiceTest extends AbstractMockitoTest {
 
     @Autowired
     private LotteIssuanceService lotteIssuanceService;
+
+    @Autowired
+    private EmailService emailService;
 
     @Autowired
     private CorpService corpService;
 
     @Test
     @Order(1)
-    @DisplayName("롯데카드 이메일 테스트")
+    @DisplayName("웰컴메일 테스트")
    void sendReceiptEmail() {
         log.info("start");
-        Corp corp = corpService.findByCorpIdx(535L);
-        // 테스트진행시 sendReceiptEmail : private -> public 변경 필요
-        lotteIssuanceService.sendReceiptEmail(corp);
+        emailService.sendWelcomeEmail("2618125793", "10");
         log.info("end");
     }
 
