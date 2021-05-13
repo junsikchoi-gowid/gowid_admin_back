b943d62dc92cc9e2915b6556a7910ca956a45813
최준식 <58801430+junsikchoi-gowid@users.noreply.github.com>
2021-05-13 15:24:11 +0900

    현금흐름 통계 오류 (#900)
    현금흐름 통계 오류 (#900)
    엑셀 , 통계 오류

--- gm-core/src/main/java/com/nomadconnection/dapp/core/domain/repository/querydsl/ConnectedMngCustomRepositoryImpl.java
+++ gm-core/src/main/java/com/nomadconnection/dapp/core/domain/repository/querydsl/ConnectedMngCustomRepositoryImpl.java
@@ -4,10 +4,7 @@ import com.nomadconnection.dapp.core.domain.common.ConnectedMng;
 import com.nomadconnection.dapp.core.domain.common.ConnectedMngStatus;
 import com.nomadconnection.dapp.core.domain.common.QConnectedMng;
 import com.nomadconnection.dapp.core.domain.corp.Corp;
-import com.nomadconnection.dapp.core.domain.res.QResAccount;
-import com.nomadconnection.dapp.core.domain.res.QResAccountHistory;
-import com.nomadconnection.dapp.core.domain.res.ResAccount;
-import com.nomadconnection.dapp.core.domain.res.ResAccountHistory;
+import com.nomadconnection.dapp.core.domain.res.*;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.JPQLQuery;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
@@ -59,7 +56,7 @@ public class ConnectedMngCustomRepositoryImpl extends QuerydslRepositorySupport 
 
         JPQLQuery<ResAccount> query = from(connectedMng)
                 .join(resAccount1).on(resAccount1.connectedId.eq(connectedMng.connectedId)
-                        .and(connectedMng.status.in(ConnectedMngStatus.ERROR, ConnectedMngStatus.NORMAL, ConnectedMngStatus.STOP)))
+                        .and(resAccount1.status.in(ResAccountStatus.ERROR, ResAccountStatus.NORMAL, ResAccountStatus.STOP)))
                 .select(resAccount1)
                 .where(connectedMng.corp.eq(corp));
 
