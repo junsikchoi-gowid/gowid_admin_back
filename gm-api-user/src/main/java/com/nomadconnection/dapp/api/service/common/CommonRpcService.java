742b98d8f7b098602a9ebc89009f12bc149a30a5
김현탁 <51900778+ml-hyuntak@users.noreply.github.com>
2021-05-25 19:52:53 +0900

    Hotfix: 로그에서 민감 정보 제외 (#920)
    * fix: 패스워드가 포함될 경우 로그 출력을 하지 않는다
    
    * fix: body는 toString만 적용하도록 변경

--- gm-api-user/src/main/java/com/nomadconnection/dapp/api/service/common/CommonRpcService.java
+++ gm-api-user/src/main/java/com/nomadconnection/dapp/api/service/common/CommonRpcService.java
@@ -11,13 +11,13 @@ import java.util.Map;
 
 @Slf4j
 @Service
-@SuppressWarnings("DuplicatedCode")
 public class CommonRpcService {
 
     public <T> ResponseEntity<T> requestApi(String url, HttpMethod httpMethod, Object bodyParams, Class<T> responseType, HttpHeaders headers, RestTemplate restTemplate) throws IOException {
         log.debug("## Request [{}] {}", httpMethod, url);
         log.debug("## Request header ==> {}", JsonUtil.generateClassToJson(headers));
-        log.debug("## Request body ==> {}", JsonUtil.generateClassToJson(bodyParams));
+        // TODO hyuntak 리팩토링 대상
+        log.debug("## Request body ==> {}", bodyParams.toString());
 
         ResponseEntity<T> response = restTemplate.exchange(url, httpMethod, new HttpEntity<>(bodyParams, headers), responseType);
         log.info("## Response ==> {}", JsonUtil.generateClassToJson(response));
